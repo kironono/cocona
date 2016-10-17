9fbc0d89e7ea7ecf7a4e7df37a5f598925631169
Junichi Kakisako <kilo.sw@gmail.com>
2016-10-17 18:47:41 +0900

    moved redis configuration

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,7 +3,5 @@ require 'resque/server'
 require 'resque-scheduler'
 require 'resque/scheduler/server'
 
-redis_config = YAML.load_file(Rails.root.join('config', 'redis.yml'))
-
-Resque.redis = Redis.new(host: redis_config[:host], port: redis_config[:port])
+Resque.redis = Redis.new(host: Settings.redis.host, port: Settings.redis.port)
 Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
