b15ec7268a9faa251903eaf38fb3fecea8b3dd75
Junichi Kakisako <kilo.sw@gmail.com>
2016-10-24 23:20:04 +0900

    Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.logger = Logger.new(config.log_path, 10, 10.megabytes)
+  config.logger = Logger.new("log/production.log", 10, 10.megabytes)
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
