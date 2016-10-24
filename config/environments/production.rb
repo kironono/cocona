f369d3e4ff08d98da52935916155ec72d3af8275
Junichi Kakisako <kilo.sw@gmail.com>
2016-10-24 23:16:26 +0900

    Fix logging rotate for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.logger = Logger.new(config.log_path, 10, 10.megabytes)
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
