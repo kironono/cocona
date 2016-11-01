f731dc630c1be464c98dc56aa037d9675b8d07da
Junichi Kakisako <kilo.sw@gmail.com>
2016-11-01 22:09:30 +0900

    Fix lograge log rotate

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   path = "#{Rails.root}/log/lograge_#{Rails.env}.log"
-  config.lograge.logger = ActiveSupport::Logger.new path
+  config.lograge.logger = ActiveSupport::Logger.new(path, 10, 10.megabytes)
   config.lograge.enabled = true
   config.lograge.keep_original_rails_log = true
   config.lograge.formatter = Lograge::Formatters::Logstash.new
