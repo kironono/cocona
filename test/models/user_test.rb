0c338388f50b0f076af4f4fe3c162e2adb4c9ec8
Junichi Kakisako <kilo.sw@gmail.com>
2016-11-06 22:46:06 +0900

    Add a test of the User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
-class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+describe User do
+
+  let(:user) { users(:normal_user) }
+
+  describe "attributes" do
+    it "name is required" do
+      proc = Proc.new do
+        user.update!(name: '')
+      end
+      proc.must_raise ActiveRecord::RecordInvalid
+    end
+
+  end
 end
